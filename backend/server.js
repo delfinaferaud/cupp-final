987f393d3f61d6b91bb6375b05078ae40e7c92db
delfinaferaud <delfina.feraud@gmail.com>
2026-08-04 19:36:50 -0300

    -

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
-app.use(cors({
-  origin: "https://cupp-app.netlify.app"
-}));
--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,9 @@ import authRoutes from './routes/auth.routes.js';
 
 const app = express(); 
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://cupp-app.netlify.app'
+}));
 app.use(express.json());
 app.use("/api/ingredients", ingredientRoutes); 
 app.use("/api/products", productRoutes); 
