d0e989edfe7c7af2f4edf1ebe794f45710aad8fe
delfinaferaud <delfina.feraud@gmail.com>
2026-08-04 19:42:55 -0300

    -

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://cupp-final.onrender.com/"
+    baseURL: "https://cupp-final.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
